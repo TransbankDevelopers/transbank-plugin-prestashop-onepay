21d9598e16dd8dd9ab679791cc204df071caacb6
Rodrigo Ayala <rayalac@gmail.com>
2018-09-13 03:15:01 -0300

    Fix module generation problems related to nonexistent folder

--- package.sh
+++ package.sh
@@ -6,4 +6,10 @@ sed -i "s/$this->version = '1.0.0'/$this->version = '${TRAVIS_TAG#"v"}'/g" onepa
 sed -i "s/\[1.0.0\]/\[${TRAVIS_TAG#"v"}\]/g" config.xml
 sed -i "s/\[1.0.0\]/\[${TRAVIS_TAG#"v"}\]/g" config_es.xml
 
-zip -r9 "onepay-$TRAVIS_TAG.zip" . -x Dockerfile composer.json composer.lock docker-compose.yml docs/\* *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* package.sh Readme.md
+mkdir .onepay
+cp -R * .onepay
+mv .onepay onepay 
+
+zip -r9 "onepay-$TRAVIS_TAG.zip" onepay -x composer.json composer.lock docker-compose.yml docs/\* package.sh README.md
+
+rm -Rf onepay
